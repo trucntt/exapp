2ef27f133dba3d9be2962569de54bdc1312dc01a
Truc Nguyen <truc.nguyen@tinypulse.com>
2017-11-09 11:41:55 +0700

    rails_apps_composer: extras

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
